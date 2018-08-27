855aa59ffc1ff90ff235cfd76ccbb1bbbc2067b6
Andrés <Andrs@DESKTOP-685B185>
2018-08-27 17:08:09 +0200

    init

---
+++ VueProfiler.Api/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc.Cors.Internal;
+
+namespace VueProfiler.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        const string POLICY = "MyPolicy";
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMiniProfiler();
+            services.AddCors();
+            //services.AddCors(option => option.AddPolicy(POLICY, builder =>
+            //{
+            //    builder.AllowAnyHeader()
+            //        .AllowAnyMethod()
+            //        .WithOrigins(new string[] { "http://localhost:8080", "http://localhost:5000" })
+            //        .WithExposedHeaders("X-MiniProfiler-Ids");
+            //}));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors(builder =>
+            {
+                builder
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials()
+                    .WithExposedHeaders("X-MiniProfiler-Ids");
+            });
+            app.UseMiniProfiler();
+            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
